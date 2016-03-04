61fb250ccc928e2785f44d292949cccd94ce1940
Kristin Greenslit <kristingreenslit@gmail.com>
2016-03-04 00:00:33 -0500

    complete v2 setup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # react-spa
 
-## Directions for 'v1' branch:
-### 'npm start' to start server
-### http://localhost:8080/webpack-dev-server/#
+## Directions for 'v2' branch:
+### 'npm run dev' to start server
+### open index.html in browser
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-var webpack = require('webpack');  
-module.exports = {  
-    entry: [
-      'webpack/hot/only-dev-server',
-      "./js/app.js"
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel", query: {presets:['react','es2015']}},
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
+var webpack = require('webpack');
+var path = require('path');
 
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
 };
 
+module.exports = config;
 
-// Tutorial: jmfurlott.com/tutorial-setting-up-a-single-page-react-web-app-with-react-router-and-webpack/
 
-// Babel issue: stackoverflow.com/questions/33460420/babel-loader-jsx-syntaxerror-unexpected-token?answertab=votes#tab-top
+// Tutorial: https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack
